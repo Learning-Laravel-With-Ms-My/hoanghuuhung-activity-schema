d0774e06446f35b8ab35b1a737645755c174dd99
Hoàng <hoanghung20030@gmail.com>
2024-03-29 09:45:30 +0700

    seeder and factory

---
+++ database/seeders/ItemsTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Item;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ItemsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        
+        // $items = [
+        //     ['name' => 'pen','price' =>100],
+        //     ['name' => 'book','price' =>1550],
+        //     ['name' => 'water','price' =>500],
+        //     ['name' => 'TV','price' =>10000],
+        //     ['name' => 'TV','price' =>10000],
+        // ];
+        // foreach ($items as $item){
+        //     Item::create($item);
+        // }
+       
+    }
+}
